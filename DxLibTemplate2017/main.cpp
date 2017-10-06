27c426fe4d2f83dee1db7fcfc6bb83fdf9714d2f
yagiri000 <yohakuari@gmail.com>
2017-10-06 11:44:54 +0900

    SetDrawScreenを一回だけ呼ぶように

--- DxLibTemplate2017/main.cpp
+++ DxLibTemplate2017/main.cpp
@@ -10,6 +10,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ChangeWindowMode(TRUE);//非全画面にセット
 	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);//画面サイズ指定
 	if (DxLib_Init() == 1) { return -1; }//初期化に失敗時にエラーを吐かせて終了
+	SetDrawScreen(DX_SCREEN_BACK);//描画先を裏画面に
 
 
 	fontHandle = CreateFontToHandle("Segoe UI", 20, 5, DX_FONTTYPE_ANTIALIASING_4X4);//フォントを読み込み
@@ -21,12 +22,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	while (ProcessMessage() == 0)
 	{
 		ClearDrawScreen();//裏画面消す
-		SetDrawScreen(DX_SCREEN_BACK);//描画先を裏画面に
 
 		GetMousePoint(&mouseX, &mouseY); //マウス座標更新
 		keyUpdate();//(自作関数)キー更新
 
-					//説明用・文字を描画：keyState配列（自作）には、各キーが何フレーム押され続けているかが入っている
+		//説明用・文字を描画：keyState配列（自作）には、各キーが何フレーム押され続けているかが入っている
 		DrawFormatStringToHandle(320, 320, GetColor(255, 255, 255), fontHandle, "Z KEY %d", keyState[KEY_INPUT_Z]);
 
 		//
