a34057d70984e214283b36791f8052ee4bc5495f
yagiri000 <yohakuari@gmail.com>
2018-06-27 20:13:50 +0900

    sample.pngとsample.wavを追加、Logを生成しないように

--- DxLibTemplate2017/main.cpp
+++ DxLibTemplate2017/main.cpp
@@ -1,40 +1,21 @@
-#include <iostream>
-#include "DxLib.h"
-
-#include "MyGlobal.h"
+#include <DxLib.h>
+#include <stdio.h>
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SetOutApplicationLogValidFlag(FALSE);//Log.txtを生成しないように設定
 	ChangeWindowMode(TRUE);//非全画面にセット
-	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);//画面サイズ指定
+	SetGraphMode(640, 480, 32);//画面サイズ指定
 	if (DxLib_Init() == 1) { return -1; }//初期化に失敗時にエラーを吐かせて終了
-	SetDrawScreen(DX_SCREEN_BACK);//描画先を裏画面に
 
-
-	fontHandle = CreateFontToHandle("Segoe UI", 20, 5, DX_FONTTYPE_ANTIALIASING_4X4);//フォントを読み込み
-
-	//
-	//ここで敵やプレイヤーのオブジェクトの実体を作る
-	//
 
 	while (ProcessMessage() == 0)
 	{
 		ClearDrawScreen();//裏画面消す
+		SetDrawScreen(DX_SCREEN_BACK);//描画先を裏画面に
 
-		GetMousePoint(&mouseX, &mouseY); //マウス座標更新
-		keyUpdate();//(自作関数)キー更新
-
-		//説明用・文字を描画：keyState配列（自作）には、各キーが何フレーム押され続けているかが入っている
-		DrawFormatStringToHandle(320, 320, GetColor(255, 255, 255), fontHandle, "Z KEY %d", keyState[KEY_INPUT_Z]);
-
-		//
-		//　ここに敵やプレイヤーのupdate,drawを書く
-		//
-
-		//左上に文字（マウスの座標）を描画
-		DrawFormatStringToHandle(20, 20, GetColor(255, 255, 255), fontHandle, "MX:%3d MY:%3d", mouseX, mouseY);
+		DrawCircle(100, 200, 30, GetColor(255, 0, 0));
 
 		ScreenFlip();//裏画面を表画面にコピー
 	}
